7453d6a5aeac69fd2467ff2bae3a362f765ca36f
Sven Mueller <mueller@synyx.de>
2019-04-03 13:52:38 +0200

    actually try to predict something

--- playground.py
+++ playground.py
@@ -16,14 +16,15 @@ tf.logging.set_verbosity(tf.logging.ERROR)
 pd.options.display.max_rows = 10
 pd.options.display.float_format = '{:.1f}'.format
 
-#dataframe = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/california_housing_train.csv", sep=",")
+# dataframe = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/california_housing_train.csv", sep=",")
 #
-#dataframe = dataframe.reindex(
+# dataframe = dataframe.reindex(
 #    np.random.permutation(dataframe.index))
-#dataframe["median_house_value"] /= 1000.0
-#dataframe
+# dataframe["median_house_value"] /= 1000.0
+# dataframe
 
 dataframe = pd.read_csv("broetchen.csv", sep=";")
+
 
 def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
     """Trains a linear regression model of one feature.
@@ -39,10 +40,10 @@ def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
     """
 
     # Convert pandas data into a dict of np arrays.
-    features = {key:np.array(value) for key,value in dict(features).items()}
+    features = {key: np.array(value) for key, value in dict(features).items()}
 
     # Construct a dataset, and configure batching/repeating.
-    ds = Dataset.from_tensor_slices((features,targets)) # warning: 2GB limit
+    ds = Dataset.from_tensor_slices((features, targets))  # warning: 2GB limit
     ds = ds.batch(batch_size).repeat(num_epochs)
 
     # Shuffle the data, if specified.
@@ -52,6 +53,7 @@ def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
     # Return the next batch of data.
     features, labels = ds.make_one_shot_iterator().get_next()
     return features, labels
+
 
 def train_model(learning_rate, steps, batch_size, input_feature):
     """Trains a linear regression model.
@@ -74,7 +76,7 @@ def train_model(learning_rate, steps, batch_size, input_feature):
 
     my_feature = input_feature
     my_feature_data = dataframe[[my_feature]].astype('float32')
-    #my_label = "median_house_value"
+    # my_label = "median_house_value"
     my_label = "bestellmenge"
     targets = dataframe[my_label].astype('float32')
 
@@ -99,7 +101,7 @@ def train_model(learning_rate, steps, batch_size, input_feature):
     plt.title("Learned Line by Period")
     plt.ylabel(my_label)
     plt.xlabel(my_feature)
-    #sample = dataframe.sample(n=300)
+    # sample = dataframe.sample(n=300)
     sample = dataframe.sample()
     plt.scatter(sample[my_feature], sample[my_label])
     colors = [cm.coolwarm(x) for x in np.linspace(-1, 1, periods)]
@@ -109,7 +111,7 @@ def train_model(learning_rate, steps, batch_size, input_feature):
     print("Training model...")
     print("RMSE (on training data):")
     root_mean_squared_errors = []
-    for period in range (0, periods):
+    for period in range(0, periods):
         # Train the model, starting from the prior state.
         linear_regressor.train(
             input_fn=training_input_fn,
@@ -157,13 +159,33 @@ def train_model(learning_rate, steps, batch_size, input_feature):
 
     print("Final RMSE (on training data): %0.2f" % root_mean_squared_error)
 
-    return calibration_data
+    return linear_regressor
 
-#dataframe["rooms_per_person"] = dataframe["total_rooms"] / dataframe["population"]
 
-calibration_data = train_model(
-    learning_rate=0.1,
+# dataframe["rooms_per_person"] = dataframe["total_rooms"] / dataframe["population"]
+
+linear_regressor = train_model(
+    learning_rate=0.05,
     steps=10,
     batch_size=1,
     input_feature="personen"
 )
+
+print("Training finished, predict something...")
+
+
+def input_fn_predict(value):
+    dataset = Dataset.from_tensor_slices({'personen': tf.constant([value])})
+    dataset = dataset.batch(1).repeat(1)
+    return dataset.make_one_shot_iterator().get_next()
+
+
+def predict(value):
+    predict_result = linear_regressor.predict(input_fn=lambda: input_fn_predict(value))
+
+    predict_result = np.array([item['predictions'][0] for item in predict_result])
+    for i in predict_result:
+        print("Predicted value for input %0.2f: %0.8f" % (value, i))
+
+
+predict(42)
