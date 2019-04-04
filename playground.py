d9c9ab45a365af3e9db617c0603289c8695ccf26
Sven Mueller <mueller@synyx.de>
2019-04-04 11:31:30 +0200

    add second feature to dataset & train on it, remove obsolete plotting

--- playground.py
+++ playground.py
@@ -2,26 +2,17 @@ from __future__ import print_function
 
 import math
 
-from IPython import display
-from matplotlib import cm
-from matplotlib import gridspec
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import sklearn.metrics as metrics
 import tensorflow as tf
+from IPython import display
 from tensorflow.python.data import Dataset
 
 tf.logging.set_verbosity(tf.logging.ERROR)
 pd.options.display.max_rows = 10
 pd.options.display.float_format = '{:.1f}'.format
-
-# dataframe = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/california_housing_train.csv", sep=",")
-#
-# dataframe = dataframe.reindex(
-#    np.random.permutation(dataframe.index))
-# dataframe["median_house_value"] /= 1000.0
-# dataframe
 
 dataframe = pd.read_csv("broetchen.csv", sep=";")
 
@@ -75,7 +66,7 @@ def train_model(learning_rate, steps, batch_size, input_feature):
     steps_per_period = steps / periods
 
     my_feature = input_feature
-    my_feature_data = dataframe[[my_feature]].astype('float32')
+    my_feature_data = dataframe[[my_feature, "temperatur"]].astype('float32')
     # my_label = "median_house_value"
     my_label = "bestellmenge"
     targets = dataframe[my_label].astype('float32')
@@ -85,7 +76,7 @@ def train_model(learning_rate, steps, batch_size, input_feature):
     predict_training_input_fn = lambda: my_input_fn(my_feature_data, targets, num_epochs=1, shuffle=False)
 
     # Create feature columns.
-    feature_columns = [tf.feature_column.numeric_column(my_feature)]
+    feature_columns = [tf.feature_column.numeric_column(my_feature), tf.feature_column.numeric_column("temperatur")]
 
     # Create a linear regressor object.
     my_optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
@@ -94,17 +85,6 @@ def train_model(learning_rate, steps, batch_size, input_feature):
         feature_columns=feature_columns,
         optimizer=my_optimizer
     )
-
-    # Set up to plot the state of our model's line each period.
-    plt.figure(figsize=(15, 6))
-    plt.subplot(1, 2, 1)
-    plt.title("Learned Line by Period")
-    plt.ylabel(my_label)
-    plt.xlabel(my_feature)
-    # sample = dataframe.sample(n=300)
-    sample = dataframe.sample()
-    plt.scatter(sample[my_feature], sample[my_label])
-    colors = [cm.coolwarm(x) for x in np.linspace(-1, 1, periods)]
 
     # Train the model, but do so inside a loop so that we can periodically assess
     # loss metrics.
@@ -128,27 +108,13 @@ def train_model(learning_rate, steps, batch_size, input_feature):
         print("  period %02d : %0.2f" % (period, root_mean_squared_error))
         # Add the loss metrics from this period to our list.
         root_mean_squared_errors.append(root_mean_squared_error)
-        # Finally, track the weights and biases over time.
-        # Apply some math to ensure that the data and line are plotted neatly.
-        y_extents = np.array([0, sample[my_label].max()])
 
-        weight = linear_regressor.get_variable_value('linear/linear_model/%s/weights' % input_feature)[0]
-        bias = linear_regressor.get_variable_value('linear/linear_model/bias_weights')
-
-        x_extents = (y_extents - bias) / weight
-        x_extents = np.maximum(np.minimum(x_extents,
-                                          sample[my_feature].max()),
-                               sample[my_feature].min())
-        y_extents = weight * x_extents + bias
-        plt.plot(x_extents, y_extents, color=colors[period])
     print("Model training finished.")
 
     # Output a graph of loss metrics over periods.
-    plt.subplot(1, 2, 2)
     plt.ylabel('RMSE')
     plt.xlabel('Periods')
     plt.title("Root Mean Squared Error vs. Periods")
-    plt.tight_layout()
     plt.plot(root_mean_squared_errors)
     plt.show()
 
@@ -163,8 +129,6 @@ def train_model(learning_rate, steps, batch_size, input_feature):
     return linear_regressor
 
 
-# dataframe["rooms_per_person"] = dataframe["total_rooms"] / dataframe["population"]
-
 linear_regressor = train_model(
     learning_rate=0.05,
     steps=10,
@@ -175,18 +139,18 @@ linear_regressor = train_model(
 print("Training finished, predict something...")
 
 
-def input_fn_predict(value):
-    dataset = Dataset.from_tensor_slices({'personen': tf.constant([value])})
+def input_fn_predict(persons, temperature):
+    dataset = Dataset.from_tensor_slices({'personen': tf.constant([persons]), 'temperatur': tf.constant([temperature])})
     dataset = dataset.batch(1).repeat(1)
     return dataset.make_one_shot_iterator().get_next()
 
 
-def predict(value):
-    predict_result = linear_regressor.predict(input_fn=lambda: input_fn_predict(value))
+def predict(persons, temperature):
+    predict_result = linear_regressor.predict(input_fn=lambda: input_fn_predict(persons, temperature))
 
     predict_result = np.array([item['predictions'][0] for item in predict_result])
     for i in predict_result:
-        print("Predicted value for input %0.2f: %0.8f" % (value, i))
+        print("Predicted value for %0.2f persons at %0.2f degrees: %0.8f" % (persons, temperature, i))
 
 
-predict(42)
+predict(42, 7)
