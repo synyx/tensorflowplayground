c6f0e7360369882916973621779ab4dfc6333c34
Sven Mueller <mueller@synyx.de>
2019-04-08 11:50:51 +0200

    update schule example

--- schule.py
+++ schule.py
@@ -6,7 +6,7 @@ from tensorflow.python.data import Dataset
 tf.logging.set_verbosity(tf.logging.ERROR)
 
 # 3. optimieren
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.3)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.05)
 optimizer = tf.contrib.estimator.clip_gradients_by_norm(optimizer, 5.0)
 
 # 1. daten laden, trainieren
@@ -20,7 +20,7 @@ def input_fn():
     labels = csv_data['bestellmenge'].astype('float32').astype('float32')
 
     dataset = Dataset.from_tensor_slices((features, labels))
-    dataset = dataset.batch(12).repeat(1)
+    dataset = dataset.batch(1).repeat(None)
 
     return dataset.make_one_shot_iterator().get_next()
 
@@ -28,10 +28,10 @@ def input_fn():
 feature_columns = [tf.feature_column.numeric_column('personen')]
 
 
-#linear_regressor = tf.estimator.LinearRegressor(feature_columns=feature_columns)
+# linear_regressor = tf.estimator.LinearRegressor(feature_columns=feature_columns)
 linear_regressor = tf.estimator.LinearRegressor(feature_columns=feature_columns, optimizer=optimizer)
 
-linear_regressor.train(input_fn)
+linear_regressor.train(input_fn, steps=12)
 
 # 2. vorhersage
 
