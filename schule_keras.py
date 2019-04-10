07d0a624641e9c9b2456a5bfc4c3b6593390a9f0
Sven Mueller <mueller@synyx.de>
2019-04-10 11:44:55 +0200

    add keras api schule sample

---
+++ schule_keras.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import, division, print_function
+
+# TensorFlow and tf.keras
+import pandas as pd
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+
+csv_data = pd.read_csv("broetchen.csv", sep=";")
+csv_data["broetchen_verzehr"] = (csv_data["bestellmenge"] - csv_data["rest"])
+
+model = keras.Sequential([
+    keras.layers.Dense(1, input_dim=1)
+])
+
+model.compile(optimizer=keras.optimizers.SGD(lr=0.05, clipnorm=5.),
+              loss='mean_squared_error',
+              metrics=['accuracy'])
+
+test_features = np.array(csv_data['personen'].astype('float32'))
+test_labels = np.array(csv_data['broetchen_verzehr'].astype('float32'))
+
+model.fit(test_features, test_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_features, test_labels)
+
+print('Test accuracy:', test_acc)
+print('Test loss:', test_loss)
+
+result = model.predict(np.array([42]), batch_size=1)
+
+print('Result:', result)
