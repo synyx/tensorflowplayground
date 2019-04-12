16088aca655c80f630d1fb4c3e715fb2ed228389
Sven Mueller <mueller@synyx.de>
2019-04-12 11:39:01 +0200

    clean up keras schule example

--- schule_keras.py
+++ schule_keras.py
@@ -1,15 +1,13 @@
 from __future__ import absolute_import, division, print_function
-
-# TensorFlow and tf.keras
 import pandas as pd
 from tensorflow import keras
-
-# Helper libraries
 import numpy as np
 
+# 1. daten importieren
 csv_data = pd.read_csv("broetchen.csv", sep=";")
 csv_data["broetchen_verzehr"] = (csv_data["bestellmenge"] - csv_data["rest"])
 
+# 2. modell erzeugen & kompilieren
 model = keras.Sequential([
     keras.layers.Dense(1, input_dim=1)
 ])
@@ -18,20 +16,19 @@ model.compile(optimizer=keras.optimizers.SGD(lr=0.008, clipnorm=5.),
               loss='mean_squared_error',
               metrics=['accuracy'])
 
-#test_features = np.array(csv_data['personen'].astype('float32'))
-test_features = [np.array(csv_data['personen'].astype('float32')), np.array(csv_data['temperatur'].astype('float32'))]
-
+# 3. features & labels bereitstellen
+test_features = np.array(csv_data['personen'].astype('float32'))
 test_labels = np.array(csv_data['broetchen_verzehr'].astype('float32'))
 
+# 4. modell trainieren
 model.fit(test_features, test_labels, epochs=75,
           validation_split=0.08333333333)
 
-test_loss, test_acc = model.evaluate(test_features, test_labels)
-
-print('Test accuracy:', test_acc)
-print('Test loss:', test_loss)
-
-#result = model.predict(np.array([42]), batch_size=1)
-result = model.predict((np.array([42]), np.array([7])), batch_size=1)
+# 5. dinge vorhersagen
+result = model.predict(np.array([42]), batch_size=1)
 
 print('Result:', result)
+
+# 6. zweites feature
+#test_features = [np.array(csv_data['personen'].astype('float32')), np.array(csv_data['temperatur'].astype('float32'))]
+#result = model.predict((np.array([42]), np.array([7])), batch_size=1)
