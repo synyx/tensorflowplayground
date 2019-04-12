f55c80ff1810fe9e700f7a3d7743d573cc38f052
Sven Mueller <mueller@synyx.de>
2019-04-12 11:52:18 +0200

    clean up image keras example

--- schule_keras_images.py
+++ schule_keras_images.py
@@ -1,34 +1,32 @@
 from __future__ import absolute_import, division, print_function
-
-# TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
-
-# Helper libraries
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
+# 1. daten laden
 fashion_mnist = keras.datasets.fashion_mnist
 
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
+# 2. label-namen
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-print(print(train_images.shape))
-print(len(train_labels))
-print(train_labels)
-print(test_images.shape)
-print(len(test_labels))
+# print(print(train_images.shape))
+# print(len(train_labels))
+# print(train_labels)
+# print(test_images.shape)
+# print(len(test_labels))
+#
+# plt.figure()
+# plt.imshow(train_images[0])
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
 
-plt.figure()
-plt.imshow(train_images[0])
-plt.colorbar()
-plt.grid(False)
-plt.show()
-
+# 3. bilddaten skalieren
 train_images = train_images / 255.0
-
 test_images = test_images / 255.0
 
 plt.figure(figsize=(10,10))
@@ -41,6 +39,7 @@ for i in range(25):
     plt.xlabel(class_names[train_labels[i]])
 plt.show()
 
+# 4. modell erzeugen & kompilieren
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(128, activation=tf.nn.relu),
@@ -51,8 +50,20 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
+# 5. modell trainieren
 model.fit(train_images, train_labels, epochs=5)
 
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 
 print('Test accuracy:', test_acc)
+
+# 6. dinge vorhersagen
+predictions = model.predict(test_images)
+print(predictions[2])
+print(np.argmax(predictions[2]))
+print(class_names[np.argmax(predictions[2])])
+print(class_names[test_labels[2]])
+
+plt.imshow(test_images[i], cmap=plt.cm.binary)
+plt.show()
+
